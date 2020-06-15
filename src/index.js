cf4c4a48b7b79499c7022468fcd76181a743cb66
Farrah Carter <carterfarraha@gmail.com>
2020-06-15 09:52:23 -0400

    Added continue button and add author form

--- src/index.js
+++ src/index.js
@@ -1,15 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import { shuffle, sample } from 'underscore';
+import AddAuthorForm from './AddAuthorForm';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn', 'Roughin It', 'Life on the Mississippi']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.png',
+    imageSource: 'Wikimedia Commons',
+    books: ['A Tale of Two Cities', 'David Copperfield',]
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+  }
+]
+
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+      author.books.some((title) =>
+        title === answer))
+  }
+}
+
+function resetState() {
+  return {
+    turnData: getTurnData(authors),
+    highlight: ''
+  };
+}
+
+let state = resetState();
+
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight = isCorrect ? 'correct' : 'wrong';
+  render();
+}
+
+function App() {
+  return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} onContinue={() => {
+    state = resetState();
+    render();
+  }} />
+}
+
+const AuthorWrapper = withRouter(({ history }) =>
+  <AddAuthorForm onAddAuthor={(author) => {
+    authors.push(author);
+    history.push('/');
+  }} />
+
 );
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <React.Fragment>
+          <Route exact path="/" component={App} />
+          <Route path="/add" component={AuthorWrapper} />
+        </React.Fragment>
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
